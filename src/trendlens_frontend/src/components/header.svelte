c5949093110074011e7bfb824ccf98784e1aad57
tommysr <47206288+tommysr@users.noreply.github.com>
2024-06-25 18:11:08 +0200

    removed unwanted comments

--- src/trendlens_frontend/src/components/header.svelte
+++ src/trendlens_frontend/src/components/header.svelte
@@ -1,14 +1,4 @@
 <script lang="ts">
-	// import {
-	// 	CommandMenu,
-	// 	Icons,
-	// 	MainNav,
-	// 	MobileNav,
-	// 	ModeToggle,
-	// } from "$lib/components/docs/index.js";
-	// import { buttonVariants } from "$lib/registry/new-york/ui/button/index.js";
-	// import { siteConfig } from "$lib/config/site.js";
-	// import { cn } from "$lib/utils.js";
 	import Navbar from './navbar.svelte';
 	import ThemeSwitch from './themeSwitch.svelte';
 </script>
@@ -23,42 +13,4 @@
 			<ThemeSwitch />
 		</div>
 	</div>
-	<!-- <MobileNav />
-		<div class="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-			<div class="w-full flex-1 md:w-auto md:flex-none">
-				<CommandMenu />
-			</div>
-			<nav class="flex items-center">
-				<a href={siteConfig.links.github} target="_blank" rel="noopener noreferrer">
-					<div
-						class={cn(
-							buttonVariants({
-								size: "sm",
-								variant: "ghost",
-							}),
-							"w-9 px-0"
-						)}
-					>
-						<Icons.gitHub class="h-4 w-4" />
-						<span class="sr-only">GitHub</span>
-					</div>
-				</a>
-				<a href={siteConfig.links.twitter} target="_blank" rel="noreferrer">
-					<div
-						class={cn(
-							buttonVariants({
-								size: "sm",
-								variant: "ghost",
-							}),
-							"w-9 px-0"
-						)}
-					>
-						<Icons.twitter class="h-3 w-3 fill-current" />
-						<span class="sr-only">X (formerly known as Twitter)</span>
-					</div>
-				</a>
-				<ModeToggle />
-			</nav>
-		</div>
-	</div> -->
 </header>
